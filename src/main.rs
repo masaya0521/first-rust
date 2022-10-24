230de283e2c7df3971e461d455ec4f9a2318b6db
masaya0521 <kaji0521km@gmail.com>
2022-10-24 11:33:15 +0900

    変数宣言とガイ列

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let x:i32 = 5;
+
+    // 要素数3の配列
+    let mut array: [i32; 3] = [1, 2, 3];
+
+    // 要素数3のベクタ
+    let mut vec: Vec<i32> = vec![1, 2, 3];
 }
